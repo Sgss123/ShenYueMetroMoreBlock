53ee38bd491d831d9b0f685b6ece54dd075d8f2d
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-03-19 23:42:00 +0800

    上传第一版

---
+++ src/main/java/net/mcreator/symtr/procedures/OpenBrowserProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.symtr.procedures;
+
+import java.util.Map;
+
+public class OpenBrowserProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
